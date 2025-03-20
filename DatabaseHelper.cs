1ffdf186a10f10add92985229fb5324ab9867163
SASCHADDD <saschadanu23@gmail.com>
2025-03-20 16:43:50 +0700

    membuat class dengan fungsi connect

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -2,3 +2,27 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 
+public class DatabaseHelper
+{
+    private MySqlConnection connection;
+
+    public DatabaseHelper()
+    {
+        string connectionString = "server=127.0.0.1;database=organisasiMahasiswa;UID=root;password=[YourPassword]";
+        connection = new MySqlConnection(connectionString);
+    }
+
+    public void Connect()
+    {
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connected to the database");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Failed to connect to the database: {ex.Message}");
+        }
+    }
+
+    
